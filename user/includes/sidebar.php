9e952f19facd7410826c8b0162afd900b83e58d1
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-15 10:08:13 +0300

    maintaining

--- user/includes/sidebar.php
+++ user/includes/sidebar.php
@@ -1,30 +1,23 @@
 <?php
-session_start();
-error_reporting(0);
-include('includes/dbconnection.php');
-if (empty($_SESSION['obcsuid'])) {
-  header('location:logout.php');
-  exit();
-} else {
+declare(strict_types=1);
+
+require_once __DIR__ . '/dbconnection.php';
+
+obcs_require_user();
+
+$uid = (int) $_SESSION['obcsuid'];
+$query = $dbh->prepare('SELECT FirstName, LastName, MobileNumber FROM tbluser WHERE ID = :uid LIMIT 1');
+$query->bindParam(':uid', $uid, PDO::PARAM_INT);
+$query->execute();
+$user = $query->fetch();
 ?>
 <div class="left-sidebar-pro">
     <nav id="sidebar">
         <div class="sidebar-header">
-            <?php
-            $uid = $_SESSION['obcsuid'];
-            $sql = "SELECT FirstName,LastName,MobileNumber from tbluser where ID=:uid";
-            $query = $dbh->prepare($sql);
-            $query->bindParam(':uid', $uid, PDO::PARAM_STR);
-            $query->execute();
-            $results = $query->fetchAll(PDO::FETCH_OBJ);
-            $cnt = 1;
-            if ($query->rowCount() > 0) {
-                foreach ($results as $row) {
-            ?>
             <a href="#"><img src="img/message/avatar.jpg" alt="" />
             </a>
-            <h3><?php echo $row->FirstName; ?> <?php echo $row->LastName; ?></h3>
-            <p><?php echo $row->MobileNumber; ?></p><?php $cnt = $cnt + 1; }} ?>
+            <h3><?php echo obcs_escape($user ? $user->FirstName : 'User'); ?> <?php echo obcs_escape($user ? $user->LastName : ''); ?></h3>
+            <p><?php echo obcs_escape($user ? $user->MobileNumber : ''); ?></p>
         </div>
         <div class="left-custom-menu-adp-wrap">
             <ul class="nav navbar-nav left-sidebar-menu-pro">
@@ -43,4 +36,4 @@ if (empty($_SESSION['obcsuid'])) {
             </ul>
         </div>
     </nav>
-</div><?php } ?>
+</div>
